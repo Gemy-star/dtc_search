a188ad8f451b0cdeb825ce6cb5918f038572ae6c
mohamed gamal <m.yassen.93@gmail.com>
2020-07-04 20:44:05 +0200

    Updates

---
+++ precache-manifest.8efebaacbe7ae60ef8bc620274d7228e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff717b518a7fe9b4a5821140e9af61e",
+    "url": "/dtc_search/index.html"
+  },
+  {
+    "revision": "e6c6fe34e9ddf359473f",
+    "url": "/dtc_search/static/css/main.391b2b7d.chunk.css"
+  },
+  {
+    "revision": "3b845b7b9bc04af15283",
+    "url": "/dtc_search/static/js/2.d8f8c669.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dtc_search/static/js/2.d8f8c669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c6fe34e9ddf359473f",
+    "url": "/dtc_search/static/js/main.c2f20cba.chunk.js"
+  },
+  {
+    "revision": "17e5d0182570a66bdb5f",
+    "url": "/dtc_search/static/js/runtime-main.06e2f747.js"
+  }
+]);
